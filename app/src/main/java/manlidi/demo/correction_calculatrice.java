758692953f15d6864a0574aba8b35717dae2635d
SANT-ANNA Marie-Didier <mariesantanna114@gmail.com>
2021-11-25 10:25:00 +0100

    'Activite(#3)'

---
+++ app/src/main/java/manlidi/demo/correction_calculatrice.java
@@ -0,0 +1,14 @@
+package manlidi.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class correction_calculatrice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_correction_calculatrice);
+    }
+}
